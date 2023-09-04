4e00a81481b9b378733c3857351b074fc8f7a579
Olimov Amir <olimovamir086@gmail.com>
2023-09-04 13:34:48 +0300

    add all method GRUD

--- app/Http/Controllers/Swagger/PostController.php
+++ app/Http/Controllers/Swagger/PostController.php
@@ -77,6 +77,66 @@ use App\Http\Controllers\Controller;
  *       ),
  *   ),
  * ),
+ * 
+ * @OA\Patch(
+ *     path="/api/posts/{post}",
+ *     summary="Обновление",
+ *     tags={"Post"},
+ *     @OA\Parameter(
+ *         description="ID post",
+ *         in="path",
+ *         name="post",
+ *         required=true,
+ *         example=2
+ *      ),
+ * 
+ *     @OA\RequestBody(
+ *         @OA\JsonContent(
+ *             allOf={
+ *              @OA\Schema(
+ *                 @OA\Property(property="title", type="string", example="Some title for edit"),
+ *                 @OA\Property(property="likes", type="integer", example=10),    
+ *           ),
+ *        }
+ *     ),
+ *   ),
+ * 
+ *     @OA\Response(
+ *         response=200,
+ *         description="ok",
+ *         @OA\JsonContent(
+ *            @OA\Property(property="data", type="object", 
+ *               @OA\Property(property="id", type="integer", example=1),
+ *               @OA\Property(property="title", type="string", example="Some title"),
+ *               @OA\Property(property="likes", type="integer", example=10),   
+ *            ),
+ *            
+ *         ),
+ *       ),
+ *   ),
+ * ),
+ * 
+ * * @OA\Delete(
+ *     path="/api/posts/{post}",
+ *     summary="Удаление",
+ *     tags={"Post"},
+ *     @OA\Parameter(
+ *         description="ID post",
+ *         in="path",
+ *         name="post",
+ *         required=true,
+ *         example=1
+ *      ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="ok",
+ *         @OA\JsonContent(
+ *            @OA\Property(property="message", type="string", example="done"),
+ *            
+ *         ),
+ *       ),
+ *   ),
+ * ),
  */
 class PostController extends Controller
 {
